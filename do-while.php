8c044944bdb2d5144701d4d4b6f4711f13cf098d
Md.Rakib Hosen <37983022+rakibthpi@users.noreply.github.com>
2021-08-25 07:53:57 +0600

    do-while loop
    loop chenge

---
+++ do-while.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="icon" href="assets/images/logo.png" type="image/gif" sizes="16x16">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+
+		$x = 0;
+		do{
+			echo "Number Print = ".$x."<br>";
+			$x++;
+		}
+		while($x<=10);
+	?>
+</body>
+</html>
---
+++ foreach.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="icon" href="assets/images/logo.png" type="image/gif" sizes="16x16">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+
+	<?php 
+		// $number = array("1"=>"Rakib","Hosen","Natok","Muvi");
+
+		$defarent = array('shuchi','kuchi','rakib','rana','rasel');
+
+		// $defarent = array(
+		// 	'tmi1'=>'shuchi',
+		// 	'tmi2'=>'kuchi',
+		// 	'tmi3'=>'rakib',
+		// 	'tmi4'=>'rana',
+		// 	'tmi5'=>'rasel'
+		// );
+
+		foreach(){
+			
+		}
+
+		
+
+	?>
+</body>
+</html>
