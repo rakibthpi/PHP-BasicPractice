1a3a395439cc58abb72f171bb1ee76a3775270e5
Md.Rakib Hosen <37983022+rakibthpi@users.noreply.github.com>
2021-08-25 16:50:22 +0600

    for loop array
    for loop array ....

--- foreach.php
+++ foreach.php
@@ -11,15 +11,15 @@
 	<?php 
 		// $number = array("1"=>"Rakib","Hosen","Natok","Muvi");
 		// intreged array 
-		// $defarent = array('shuchi','kuchi','rakib','rana','rasel');
+		$defarent = array('shuchi','kuchi','rakib','rana','rasel');
 
-		// $langth = count($defarent);
-		// echo $langth;
-		// for ($i=0; $i < $langth; $i++) { 
-		// 	echo $defarent[$i].",<br>";
-		// }
+		$langth = count($defarent);
+		echo $langth;
+		for ($i=0; $i < $langth; $i++) { 
+			echo $defarent[$i].",<br>";
+		}
 
-
+		echo "<br><br><br>";
 		// Array How many Type =>Specifying with array()
 
 		$defarent = array(
@@ -32,6 +32,41 @@
 
 		foreach($defarent as $key=>$newname){
 			echo $key." ".$newname.'<br>';
+		}
+		echo "<br><br><br>";
+
+		// Multidumentional Array 
+
+		$motor = array(
+
+				array('Hero', 'hero honda+', 'Hero Spander+', 'Hank'),
+				array('Bajaj', 'CT100', 'Discover 25', 'Discover 100', 'palsear','Daying'),
+				array('Yamaha', 'yamaha FZ', 'yamaha saluto', 'yamaha fazer'),
+				array('TVS', 'TVS metro', 'TVS Apache', 'Tvs Stryker')
+		);
+		$motorrow = count($motor);
+
+		// $subrow = count();
+		// echo $subrow;
+
+
+
+
+
+		// Multi dymentional array + for loop 
+		for($row=0;$row<$motorrow;$row++){
+			echo "Row Number = $row";
+			$motorcol = count($motor[$row]);
+			echo "<ul>";
+
+			for($col=0;$col<$motorcol;$col++){
+				echo '<li><a href="#">';
+				$adata = $motor[$row][$col];
+				echo $adata;
+				echo '</li></a>';
+			}
+
+			echo "</ul>";
 		}
 
 		
