f700c3e8827189445c6dba1597ba21588d0f9f5a
Md.Rakib Hosen <37983022+rakibthpi@users.noreply.github.com>
2021-08-27 21:55:48 +0600

    string function
    string function

--- string-function.php
+++ string-function.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-	<!-- <link rel="icon" href="assets/images/logo.png" type="image/gif" sizes="16x16"> -->
+	<!-- <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> -->
+	<link rel="icon" href="assets/images/logo.png" type="image/gif" sizes="16x16">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
 </head>
@@ -20,7 +20,23 @@
 		}
 		echo gameing();
 		echo "<br><br><br>";
-		echo "This is a string Function";
+		echo "This is a string Function start!!!";
+
+		// Back / user korar jonnno ata 
+		$str = "<br>Hello World!";
+		echo addcslashes($str,"W")." <br><br>"; 
+
+		$stra = "Hello World!";
+		echo bin2hex($stra)." <br><br>";
+
+		$str = "Hello World! <br><br>";
+		echo $str . "<br> <br><br>";
+		echo chop($str,"Hello");
+
+
+
+
+
 
 	?>
 </body>
